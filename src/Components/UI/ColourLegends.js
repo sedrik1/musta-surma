93a379bd8b9826cb3b9f28a0d24384c5633e2fe7
sedrik1 <32675171+sedrik1@users.noreply.github.com>
2020-10-17 15:17:11 +0300

    Poistettu turha tiedosto ja väritietosivulaatikon ulkoisen ilmeen parantelu

--- src/Components/UI/ColourLegends.js
+++ src/Components/UI/ColourLegends.js
@@ -7,68 +7,68 @@ const ColourLegends = () => {
 			<ul id="colours-list">
 				<li>
 					<span>v. 1346</span>
-					<svg height="20" width="30">
-						<circle cx="15" cy="10" r="7" fill="#ff0000" />
+					<svg height="20" width="15">
+						<circle cx="7" cy="10" r="5" fill="#ff0000" />
 					</svg>
 				</li>
 				<li>
 					<span>v. 1347</span>
-					<svg height="20" width="30">
-						<circle cx="15" cy="10" r="7" fill="#f79f66" />
+					<svg height="20" width="15">
+						<circle cx="7" cy="10" r="5" fill="#f79f66" />
 					</svg>
 				</li>
 				<li>
 					<span>v. 1348</span>
-					<svg height="20" width="30">
-						<circle cx="15" cy="10" r="7" fill="#fc5d23" />
+					<svg height="20" width="15">
+						<circle cx="7" cy="10" r="5" fill="#fc5d23" />
 					</svg>
 				</li>
 				<li>
 					<span>v. 1349</span>
-					<svg height="20" width="30">
-						<circle cx="15" cy="10" r="7" fill="#fc2356" />
+					<svg height="20" width="15">
+						<circle cx="7" cy="10" r="5" fill="#fc2356" />
 					</svg>
 				</li>
 				<li>
 					<span>v. 1350</span>
-					<svg height="20" width="30">
-						<circle cx="15" cy="10" r="7" fill="#fc23c2" />
+					<svg height="20" width="15">
+						<circle cx="7" cy="10" r="5" fill="#fc23c2" />
 					</svg>
 				</li>
 				<li>
 					<span>v. 1351</span>
-					<svg height="20" width="30">
-						<circle cx="15" cy="10" r="7" fill="#c923fc" />
+					<svg height="20" width="15">
+						<circle cx="7" cy="10" r="5" fill="#c923fc" />
 					</svg>
 				</li>
 				<li>
 					<span>v. 1352</span>
-					<svg height="20" width="30">
-						<circle cx="15" cy="10" r="7" fill="#5d23fc" />
+					<svg height="20" width="15">
+						<circle cx="7" cy="10" r="5" fill="#5d23fc" />
 					</svg>
 				</li>
 				<li>
 					<span>v. 1353</span>
-					<svg height="20" width="30">
-						<circle cx="15" cy="10" r="7" fill="#2356fc" />
+					<svg height="20" width="15">
+						<circle cx="7" cy="10" r="5" fill="#2356fc" />
 					</svg>
 				</li>
 				<li className="odd">
 					<span>Maareitit</span>
-					<svg height="20" width="30">
-						<circle cx="15" cy="10" r="7" fill="#8a0303" />
+					<svg height="20" width="15">
+						<circle cx="7" cy="10" r="5" fill="#8a0303" />
 					</svg>
 				</li>
 				<li className="odd">
 					<span>Merireitit</span>
-					<svg height="20" width="30">
-						<circle cx="15" cy="10" r="7" fill="#003366" />
+					<svg height="20" width="15">
+						<circle cx="7" cy="10" r="5" fill="#003366" />
 					</svg>
 				</li>
 				<li className="odd">
 					<span>Kultaisen ordan raja</span>
-					<svg height="20" width="30">
-						<circle cx="15" cy="10" r="7" fill="#FFD700" />
+					<svg height="20" width="15">
+						<circle cx="7" cy="10" r="5" fill="#FFD700" />
 					</svg>
 				</li>
 			</ul>
