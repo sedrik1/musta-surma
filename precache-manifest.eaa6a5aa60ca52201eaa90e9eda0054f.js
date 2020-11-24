c236cd1098fd253f4beec571d3f02aaae376a688
sedrik1 <32675171+sedrik1@users.noreply.github.com>
2020-11-24 17:28:15 +0200

    Updates

---
+++ precache-manifest.eaa6a5aa60ca52201eaa90e9eda0054f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a59fbde73b658a80cda59ffffeb1df",
+    "url": "/musta-surma/index.html"
+  },
+  {
+    "revision": "79218318f22dc777f5ee",
+    "url": "/musta-surma/static/css/main.4222d66a.chunk.css"
+  },
+  {
+    "revision": "0d9f4e78e413a8144bed",
+    "url": "/musta-surma/static/js/2.cec20811.chunk.js"
+  },
+  {
+    "revision": "5ff52529f2921c1e05a1398c3d17beaa",
+    "url": "/musta-surma/static/js/2.cec20811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79218318f22dc777f5ee",
+    "url": "/musta-surma/static/js/main.b4beb1fe.chunk.js"
+  },
+  {
+    "revision": "b74c98c416f127c10c19",
+    "url": "/musta-surma/static/js/runtime-main.49999820.js"
+  }
+]);
